8847d58d559cae414e1012da20f77995fd79abde
mhdarifmrstn <mhdarifmrstn@yahoo.com>
2023-06-17 15:06:52 +0900

    remove unused middlewares

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,5 @@
 import { NewMessageEvent } from "telegram/events/index.js";
 import uptimeHandler from "./uptime.js";
-import logMiddleware from "../middlewares/log.js";
 
 type Handler = (event: NewMessageEvent) => void;
 type Middleware = Handler;
@@ -8,8 +7,5 @@ type Extra = { middlewares: Middleware[] };
 type RegisteredCommand = [string[], Handler, Extra?];
 
 const registeredCommands: RegisteredCommand[] = [[["uptime"], uptimeHandler, { middlewares: [] }]];
-registeredCommands.forEach((cmd) => {
-  cmd[2]?.middlewares.push(logMiddleware);
-});
 
 export default registeredCommands;
--- src/middlewares/deleteMessage.ts
+++
@@ -1,9 +0,0 @@
-import { NewMessageEvent } from "telegram/events/index.js";
-
-async function deleteMessageMiddleware(event: NewMessageEvent) {
-  try {
-    await event.message.delete();
-  } catch {}
-}
-
-export default deleteMessageMiddleware;
