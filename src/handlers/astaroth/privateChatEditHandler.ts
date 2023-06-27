50f7ec664e991446bbb055082f3571edbe4c78c2
mhdarifmrstn <mhdarifmrstn@yahoo.com>
2023-06-27 21:57:05 +0900

    fix timeout card message not edited

--- src/handlers/astaroth/privateChatEditHandler.ts
+++ src/handlers/astaroth/privateChatEditHandler.ts
@@ -1,29 +1,31 @@
 import { NewMessageEvent } from "telegram/events/index.js";
 import globalState from "../../services/globalState.js";
 import debug from "../../services/debug.js";
+import getCurrentGame from "../../extra/getCurrentGame.js";
 
 async function privateChatEditHandler(event: NewMessageEvent) {
   const messageText = event.message.message;
   const timeoutKeepIdMessage = "Waktu habis!\nRow";
   const timeoutKeepEnMessage = "Time's up!\nRow";
   const client = event.client;
-  const chatId = event.chatId?.toString();
 
-  if (!client || !chatId) return;
+  if (!client) return;
 
   const playerId = client._selfInputPeer?.userId.toString() || "";
-  const chat = globalState.registeredChats[chatId];
   const player = globalState.registeredPlayers[playerId];
+  const arePlayed = getCurrentGame(playerId);
 
-  if (chat && player) {
+  if (player && arePlayed) {
+    const chatIdTheyArePlayed = arePlayed;
+
     if (messageText.includes(timeoutKeepIdMessage) || messageText.includes(timeoutKeepEnMessage)) {
       const selectedRow = messageText.match(/\d/);
       const newText = `row ${selectedRow} telah dipilih oleh astaroth`;
-      const waitingMessageId = globalState.keepCard.task[chatId.toString()].waitingMessageId;
+      const waitingMessageId = globalState.keepCard.task[chatIdTheyArePlayed].waitingMessageId;
       const bot = globalState.keepCard.bot;
 
       debug(`astaroth choose ${player.name} row ${selectedRow} because it has passed the time limit`);
-      await bot.editMessage(chatId, { message: waitingMessageId, text: newText });
+      await bot.editMessage(chatIdTheyArePlayed, { message: waitingMessageId, text: newText });
     }
   }
 }
