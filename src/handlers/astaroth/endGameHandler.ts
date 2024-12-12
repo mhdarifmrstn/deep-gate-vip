30ca577316717c5dcd6b8016adf5d8840bcba2b2
mhdarifmrstn <mhdarifmrstn@yahoo.com>
2024-12-12 12:08:35 +0700

    fix endGameHandler limit execution not applied

--- src/handlers/astaroth/endGameHandler.ts
+++ src/handlers/astaroth/endGameHandler.ts
@@ -4,9 +4,8 @@ import debug from "../../services/debug.js";
 import _ from "lodash";
 import sendStartCommand from "../../services/sendStartCommand.js";
 
-async function endGameHandler(event: NewMessageEvent) {
-  await _.throttle(() => _endGameHandler(event), 5000)();
-}
+const endGameHandler = _.throttle((event: NewMessageEvent) => _endGameHandler(event), 5000);
+
 async function _endGameHandler(event: NewMessageEvent) {
   const chatId = event.chatId?.toString() || "";
   const chat = globalState.registeredChats[chatId];
